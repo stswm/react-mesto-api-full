a9130ca9390fd03c29d322c8674439949385c1a5
Oleg Azadov <oazadov@gmail.com>
2022-06-09 20:52:51 +0300

    somefix

--- backend/app.js
+++ backend/app.js
@@ -31,35 +31,35 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 // app.use(cors(corsOptions));
 
-// const allowedCors = [
-//   'https://praktikum.tk',
-//   'http://praktikum.tk',
-//   'localhost:3000',
-//   'http://http://stswm.nomoredomains.xyz',
-//   'https://http://stswm.nomoredomains.xyz',
-//   'http://api.stswm.nomoreparties.sbs',
-//   'https://api.stswm.nomoreparties.sbs',
-// ];
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'http://http://stswm.nomoredomains.xyz',
+  'https://http://stswm.nomoredomains.xyz',
+  'http://api.stswm.nomoreparties.sbs',
+  'https://api.stswm.nomoreparties.sbs',
+];
 
 app.use(express.json());
 app.use(requestLogger);
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   next();
-// });
-app.use(cors());
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+// app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
